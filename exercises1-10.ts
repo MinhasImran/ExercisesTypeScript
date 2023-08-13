32baf7b41ac1c91145e0b3ebd38cef72e33e8cc4
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-13 19:48:12 +0500

    Add files via upload

---
+++ exercises1-10.ts
@@ -0,0 +1,57 @@
+//Assign-01 Install Node.js, TypeScript and VS Code
+//Done
+let Q="Question Number ";
+console.log (Q+2);
+//Assign-02 Store a person's name in variable and print a message
+let pn="Imran Hussain";
+let pn1="Assalam o Alaikum "+ pn+ " would you like to stay here?";
+console.log(pn1);
+//Assign-03 defin variable and change cases
+console.log (Q+3);
+let upcs= pn.toUpperCase();
+console.log(upcs);
+let lwcs= pn.toLowerCase();
+console.log(lwcs);
+//title case is not clear
+//Assgin=04 write quote of any famous person in quotations("")
+console.log (Q+4);
+let fq="Hazrat Umar (r.a) said,";
+let fq1=fq+ (' "I love Holy Prophet more than my life."');
+console.log (fq1);
+//Assign-05 Repeat with famous_Person and Message variables
+console.log (Q+5);
+let famous_person="Hazrat Umar (r.a) said,";
+let message=famous_person+ (' "I love Holy Prophet more than my life."');
+console.log(message);
+// white spaces use '\n for vertical space, it works like enter a new line `\t for horizontal space it works like tab
+let name1="Muhammad Ali Jinnah";
+console.log(`\n ${name1}\n`);
+console.log(`\n \n ${name1}\n \n`);
+console.log(`\t ${name1}\t`);
+console.log(`\t \t ${name1}\t \t`);
+//Assign-07 write addtion, substraction, multiplication & div
+console.log (Q+7 + " Add, substract, multiply and divide");
+let n=8
+console.log(n+0 + " =addition");
+console.log(n-0 + " =substraction");
+console.log(n*1 + " =multiplication");
+console.log(n/1 + " =division");
+console.log (Q+8+ " Create four lines");
+let l="="
+let ln="-"
+const line = l.repeat(80);
+const line1 = ln.repeat(80);
+console.log(line);
+console.log(line1);
+console.log(`\t \t \t \t` + (5+3));
+console.log(line1);
+console.log(line);
+console.log (Q+9 + " describe favorite number and print");
+let n1=7
+let n2=n1 + " is my favourite number";
+console.log(n2)
+console.log (Q+10 + " write comments");
+//My name Imran Hussain
+/* My Name is Imran Hussain
+and today is August 12, 2023 */
+console.log("Comment added, Name is Imran Hussain and date is August 12, 2023");
