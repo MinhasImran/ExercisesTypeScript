8e6a7e46615ea6cf50d4a31bae84d55975054d15
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-27 22:27:45 +0500

    Exercise 4 - Batch 47 (Morning) Lahore

---
+++ exercise04.ts
@@ -0,0 +1,9 @@
+//Assgin=04 write quote of any famous person in quotations("")
+console.log ("Qustion Number 4 Famous person's quotations");
+let fq="Hazrat Umar (r.a) said,";
+let fq1=fq+ (' "I love Holy Prophet more than my life."');
+console.log (fq1);
+
+let f_person:string="Hazrat Umar (r.a) said"
+let mess:string="I love Holy Prophet more than my life.";
+console.log(`${f_person}, ${mess}`);
