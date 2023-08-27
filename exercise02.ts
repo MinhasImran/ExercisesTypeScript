e2f33d62e7b66083dc5d8f3cca76af61e7f2165d
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-27 21:52:12 +0500

    Exercise 02 - Batch 47 (Morning) Lahore

---
+++ exercise02.ts
@@ -0,0 +1,10 @@
+//Assign-01 Install Node.js, TypeScript and VS Code
+//Done
+let Q="Question Number ";
+console.log (Q+2);
+//Assign-02 Store a person's name in variable and print a message
+let pn="Imran Hussain";
+let pn1="Assalam o Alaikum "+ pn+ " would you like to stay here?";
+let pn2=`Assalam o Alaikum ${pn}, would you like to stay here?`;
+console.log(pn1);
+console.log(pn2);
