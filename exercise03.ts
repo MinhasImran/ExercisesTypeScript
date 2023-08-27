ce29638dfb62f27b987d89bec69ce3e6fc606f84
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-27 22:18:26 +0500

    Add files via upload

---
+++ exercise03.ts
@@ -0,0 +1,12 @@
+//Assign-03 defin variable and change cases
+console.log ("Question Number 3");
+let pnn="Imran Hussain"
+let upcs= pnn.toUpperCase();
+
+console.log(upcs);
+console.log(
+    `Hello ${pnn.toUpperCase()}, would your like to eat something?`
+);
+let lwcs= pnn.toLowerCase();
+console.log(lwcs);
+//title case is not clear
