b8cffe5b557c49b4c13c627fe38cac2b4e40f3b0
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-27 22:33:55 +0500

    Exercise 05 - Batch 47 (Morning) Lahore

---
+++ exercise05.ts
@@ -0,0 +1,5 @@
+//Assign-05 Repeat with famous_Person and Message variables
+console.log("Question Number 5");
+let famous_person="Hazrat Umar (r.a) said,";
+let message=famous_person+ (' "I love Holy Prophet more than my life."');
+console.log(message);
