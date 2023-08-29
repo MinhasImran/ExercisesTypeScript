1ae10f118dada027b681c1c2cc1f7bd6af1e900b
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-29 22:00:37 +0500

    Batch-47 (M) Lahore - Use array and greet friends

---
+++ exercise12.ts
@@ -0,0 +1,4 @@
+let names1:string[]=[`Imran`, `Adnan`, `Usman`, `Soban`];
+let mes:string=(`Assalam o Alaikum. You are a lovely person Mr. `);
+console.log (mes+ names1 [0]);
+console.log (mes+ names1 [1]);
