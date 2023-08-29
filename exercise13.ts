5e7f3ff18a167fa052fb7f0a9d7df20476077804
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-29 22:12:35 +0500

    Batch 47(M) Lah - List statmnt, fv veh, din invit

---
+++ exercise13.ts
@@ -0,0 +1,8 @@
+let auto=(" Range Rover ")
+let state=["I would love to drive", "I would like to travel around the world on", "The strongest vehicle to travel on mountain is"];
+console.log(state [0]+auto);
+console.log(state [2]+auto);
+let names2:string[]=[`Imran`, `Adnan`, `Usman`, `Soban`];
+let mes1:string=(" Assalam o Alaikum I am happy on your successful completion of business orders and establishing a great business empire. I would like to invite your at dinner on August 23, 2023 at 9:00 PM, at my home. Waiting for your response. Regards Imran");
+console.log("Dear Mr. "+ names2[0]+mes1);
+console.log("Dear Mr. "+ names2[2]+mes1);
