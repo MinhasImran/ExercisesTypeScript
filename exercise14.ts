69102e13e470f58ce7807c7d26127587ca1fd1b0
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-29 22:16:41 +0500

    Batch 47(M) LHR - Guest list and invitation

---
+++ exercise14.ts
@@ -0,0 +1,10 @@
+let glist: Array<string> = [
+    "Imran Hussain",
+    "Adnan Ibrar",
+    "Allam Iqbal"
+];
+//Loop for each guest name
+glist.forEach((guestName) =>
+{
+console.log(`Dear ${guestName}, you are invited to join the conference.`)
+})
