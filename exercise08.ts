c91186e4b531b57f96f5efb7a4ef3c0b827b5301
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-29 21:22:18 +0500

    Exercise 08 - Batch 47(M) Lahore - Create 4 lines

---
+++ exercise08.ts
@@ -0,0 +1,9 @@
+let l="="
+let ln="-"
+const line = l.repeat(80);
+const line1 = ln.repeat(80);
+console.log(line);
+console.log(line1);
+console.log(`\t \t \t \t` + (5+3));
+console.log(line1);
+console.log(line);
