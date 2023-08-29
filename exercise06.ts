619206029c24e0f026c52fa9c14b600ac1260330
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-29 19:30:02 +0500

    Exercise 06 - Batch 47 (M) Lahore White Spaces

---
+++ exercise06.ts
@@ -0,0 +1,10 @@
+console.log ("Exercise Number - 06");
+//use white spaces \n \t
+let name1="Muhammad Ali Jinnah";
+console.log(`\n ${name1}\n`);
+console.log(`\n \n ${name1}\n \n`);
+console.log(`\t ${name1}\t`);
+console.log(`\t \t ${name1}\t \t`);
+//Other method
+let name2:string="   Muhammad \n   Ali \t  Jinnah\t The \t Leader";
+console.log(name2);
