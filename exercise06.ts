242aa5f8e0598e3475de0882a292113f7e7e62c6
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-29 19:28:47 +0500

    Delete exercise06.ts

--- exercise06.ts
+++
@@ -1,7 +0,0 @@
-console.log ("Exercise Number - 06");
-//use white spaces \n \t
-let name1="Muhammad Ali Jinnah";
-console.log(`\n ${name1}\n`);
-console.log(`\n \n ${name1}\n \n`);
-console.log(`\t ${name1}\t`);
-console.log(`\t \t ${name1}\t \t`);
