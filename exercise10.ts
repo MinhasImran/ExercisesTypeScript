a093e336bd4620e61a8b454677d53a7a70f5d937
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-08-29 21:45:47 +0500

    Batch 47 (M) Lahore - Write comments

---
+++ exercise10.ts
@@ -0,0 +1,7 @@
+//My name Imran Hussain
+/* Imran Hussain, August 12, 2023
+This program takes two number and return their sum */
+const sum=(num1:number, num2: number)=>{
+    return num1+num2;
+}
+console.log(sum (5, 9));
