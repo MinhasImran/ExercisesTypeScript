b1ba0b8bebac542e6e244656078a267653faf93a
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-09-10 19:30:21 +0500

    Exe 18 Batch 47(M) LHR five fav places and sorting

---
+++ exercise18.ts
@@ -0,0 +1,17 @@
+//think five plances, store them in array, print, sort in order, sort in reverse order .....
+let places: string[]=['Zambia', 'Greece', "Corfu", "Santorini", 'Norway'];
+
+console.log(places);
+console.log([...places].sort());
+console.log(places);
+console.log([...places].sort().reverse());
+console.log(places);
+places.reverse();
+console.log(places);
+places.reverse();
+console.log(places);
+places.sort()
+console.log(places);
+places.reverse()
+console.log(places);
+
