c5dec03aa52eeaf9cb210fc0e59de72e39a6d444
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-09-17 16:20:03 +0500

    Make a list of mountains & write a program

---
+++ exercise20.ts
@@ -0,0 +1,15 @@
+let rivers: string[]=['Indus', 'Chenab', 'Satluj', 'Bayas', 'Ravi']
+console.log(rivers[3] + " flows through the Punjab")
+let countries:string[]=['Pakistan', 'Saudi Arabia', 'Egypt', 'Jordan']
+console.log(countries [2] + ` is an Islamic country`)
+let cities:string[]=['Islamabad', 'Lahore', 'Karachi', 'Macca', 'Madina']
+let languages: string[]=['Urdu', 'Arabic', 'Persian', 'Russian', 'Polish']
+let mountains: string []= ['Karakoram', 'Rakaposhi', 'K2', 'Mount Everest', 'Himalya' ]
+
+console.log("List of Pakistani Mountains")
+//Write program to create list of mountains
+for (let peaks of mountains)
+{
+    console.log(peaks)
+}
+console.log(mountains[1] + " Mountain is situated in GB province of Pakistan")
