ebf638ec9e8833750e8a28520043b4912dd0301a
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-09-17 12:42:13 +0500

    Batch 47(M) Lahore - Exe 19 Import from other file

---
+++ exercise19.ts
@@ -0,0 +1,5 @@
+//Working with program exe 14 through 18 print message for dinner
+//importing guest list from exe 14
+import{glist4} from '../exercise14'
+console.log('\nPrinting message indicating guests')
+console.log(`Our final list of guests is ${glist4.length} of exercise 14`)
