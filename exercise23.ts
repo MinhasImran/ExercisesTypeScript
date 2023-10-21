88feba3023dd0c08732b1d8387696005ed882136
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-10-21 20:42:23 +0500

    Write conditional tests - 10 examples
    Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
    let car = 'subaru';
    console.log("Is car == 'subaru'? I predict True.")
    console.log(car == 'subaru')
    • Look closely at your results, and make sure you understand why each line evaluates to True or False.
    • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.

---
+++ exercise23.ts
@@ -0,0 +1,30 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+let car = "subaru";
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == "subaru"); //true
+console.log("Is car == 'Toyota'? I predict False.");
+console.log(car == "Toyota"); //false
+console.log("Is car not == 'subaru'? I predict False.");
+console.log(car !== "subaru");
+console.log("Is car not == 'Toyota'? I predict true.");
+console.log(car !== "Toyota");
+let age = 35;
+console.log("Is age > 30? I predict, True");
+console.log(age > 30);
+console.log("Is age < 30? I predict, False");
+console.log(age < 30);
+let station = "Madina";
+console.log("Is station == 'Madina'? I predict True.");
+console.log(station == "Madina");
+console.log("Is station !== 'Madina'? I predict, False.");
+console.log(car !== "Madina");
+let year = 2023;
+console.log("Is year == 2023? I predict True.");
+console.log(year == 2023);
+console.log("Is year == 2024? I predict False.");
+console.log(year == 2024);
