f69490b680a054515b0c2a375497a99a64b9dc70
MinhasImran <140253068+MinhasImran@users.noreply.github.com>
2023-10-26 20:25:53 +0500

    Function and call summary of sandwich order
    Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.

---
+++ exercise44.ts
@@ -0,0 +1,20 @@
+/*Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. 
+The function should have one parameter that collects as many items as the function call provides, 
+and it should print a summary of the sandwich that is being ordered. 
+Call the function three times, using a different number of arguments each time.*/
+function makeSandwich(...items: string[]): void {
+  console.log("Sandwhich Summary");
+  if (items.length === 0) {
+    console.log("   - You orderded no sandwich. Please add your choice.");
+  } else {
+    items.forEach((item, index) => {
+      console.log(`    ${index + 1}. ${item}`);
+    });
+  }
+  console.log("\n");
+}
+//Call the function
+makeSandwich();
+makeSandwich("Chicken", "BBQ", "Egg");
+makeSandwich("Shami", "Chicken spread");
+makeSandwich("Cheese", "Cream and Butter");
